579c44c438c87f84b4970930c7bdd6182d4cd980
mpovill- <mpovill-@pop-os.localdomain>
2023-08-18 16:27:42 +0200

    ft_memset.c fixed.

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mpovill- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 16:04:09 by mpovill-          #+#    #+#             */
-/*   Updated: 2023/08/18 16:06:16 by mpovill-         ###   ########.fr       */
+/*   Updated: 2023/08/18 16:26:59 by mpovill-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	*memset(void *s, int c, size_t n)
 {
-	size_t	count;
+	size_t			count;
+	unsigned char	*ptr;
 
+	ptr = s;
 	count = 0;
 	while (count < n)
 	{
-		*(s + count) = c;
+		*(ptr + count) = (unsigned char)c;
 		count++;
 	}
+	return (s);
 }
