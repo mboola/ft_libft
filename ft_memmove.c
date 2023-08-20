0f3c1de20b74ef1f4a8e5b9db012d394fe180bd6
mpovill- <mpovill-@pop-os.localdomain>
2023-08-20 13:22:02 +0200

    ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpovill- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/19 12:14:47 by mpovill-          #+#    #+#             */
+/*   Updated: 2023/08/20 13:21:18 by mpovill-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*memmove(void *dest, const void *src, size_t n)
+{
+	size_t				count;
+	unsigned char		*ptr_dest;
+
+	ptr_dest = dest;
+	if (dest > src)
+	{
+		count = n;
+		while (count > 0)
+		{
+			count--;
+			*(ptr_dest + count) = *(unsigned char *)(src + count);
+		}
+		*(ptr_dest + count) = *(unsigned char *)(src + count);
+	}
+	else
+	{
+		count = 0;
+		while (count < n)
+		{
+			*(ptr_dest + count) = *(unsigned char *)(src + count);
+			count++;
+		}
+	}
+	return (dest);
+}
