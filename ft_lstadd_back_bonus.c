83581641f67401dbed3219bf5ac4c0fe1c08b3b6
mpovill- <mpovill-@pop-os.localdomain>
2023-08-26 15:11:15 +0200

    ft_lstadd_back_bonus.c added

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpovill- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/26 14:32:47 by mpovill-          #+#    #+#             */
+/*   Updated: 2023/08/26 14:41:02 by mpovill-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (lst == NULL)
+		return ;
+	if (*lst == NULL)
+		*lst = new;
+	else
+		ft_lstlast(*lst)->next = new;
+}
