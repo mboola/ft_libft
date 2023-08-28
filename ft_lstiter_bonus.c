978899b38ba483ed2a75b04e4f9fd491ae780a49
mpovill- <mpovill-@pop-os.localdomain>
2023-08-28 21:05:14 +0200

    ft_lstiter_bonus.c created and tested

---
+++ ft_lstiter_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpovill- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 19:58:26 by mpovill-          #+#    #+#             */
+/*   Updated: 2023/08/28 20:34:47 by mpovill-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (lst == NULL)
+		return ;
+	while(lst->next != NULL)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+	f(lst->content);
+}
