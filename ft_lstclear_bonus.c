1efb14735be572c8b8d53c4d76aa176b34732411
mpovill- <mpovill-@pop-os.localdomain>
2023-08-28 21:04:50 +0200

    ft_lstclear_bonus.c created and tested with valgrind

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpovill- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 19:48:45 by mpovill-          #+#    #+#             */
+/*   Updated: 2023/08/28 20:24:00 by mpovill-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+	if (lst == NULL || *lst == NULL)
+		return ;
+	tmp = *lst;
+	while (tmp->next != NULL)
+	{
+		*lst = (*lst)->next;
+		ft_lstdelone(tmp, del);
+		tmp = *lst;
+	}
+	ft_lstdelone(tmp, del);
+}
